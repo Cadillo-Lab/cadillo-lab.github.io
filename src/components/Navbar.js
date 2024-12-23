8f5a80dfc043b216af563846648e9438dd1074b1
sanskarsri26 <113485640+sanskarsri26@users.noreply.github.com>
2024-12-23 12:54:56 -0700

    homepage changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Navbar.css'; // Correct path to Navbar.css
-import asuLogo from '../assets/asulogo.png'; // Correct path to the logo file
-
+import '../styles/Navbar.css'; 
+import asuLogo from '../assets/asulogo.png'; 
+import banner from '../assets/banner1.jpg';
 
 const Navbar = () => {
     return (
         <header className="navbar">
+            {/* Left: ASU Logo */}
             <div className="navbar-left">
                 <img src={asuLogo} alt="ASU Logo" className="logo" />
-                <span className="lab-name">Cadillo Lab</span>
             </div>
+
+            {/* Center: Banner */}
+            <div className="navbar-banner">
+                <img src={banner} alt="Banner Logo" className="banner" />
+            </div>
+
+            {/* Right: Navigation Links */}
             <nav className="navbar-links">
                 <ul>
-                <li><Link to="/">Home</Link></li>
+                    <li><Link to="/">Home</Link></li>
                     <li><Link to="/research">Research</Link></li>
                     <li><Link to="/team">Team</Link></li>
                     <li><Link to="/publications">Publications</Link></li>
