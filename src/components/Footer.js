543d0cb01ec659ada692209b1b13454f03200b6f
sanskarsri26 <113485640+sanskarsri26@users.noreply.github.com>
2024-12-23 22:47:13 -0700

    footer push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,33 @@
-import React, { useEffect } from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Footer.css";
-
 const Footer = () => {
-       
+  return (
+    <footer className="footer">
+      <nav className="footer-links">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/research">Research</Link>
+          </li>
+          <li>
+            <Link to="/team">Team</Link>
+          </li>
+          <li>
+            <Link to="/publications">Publications</Link>
+          </li>
+          <li>
+            <Link to="/join-us">Join Us</Link>
+          </li>
+          <li>
+            <Link to="/collaborations">Collaborations</Link>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
