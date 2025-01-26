d49389b2d58666fd45858fd5737992701ee1d9fb
Sanskar Srivastava <113485640+sanskarsri26@users.noreply.github.com>
2025-01-25 22:26:19 -0700

    collaboration styling done

--- src/components/Collaborationss.js
+++ src/components/Collaborationss.js
@@ -46,50 +46,58 @@ const Collaborationss = () => {
             <h3>
               Microbiology EducatioN, Training and OutReach at ASU (MENTOR@ASU)
             </h3>
-            <p>
+            <p className="para11" style={{ lineHeight: "1.2" }}>
               This is a student-centered Inquiry-Base Learning effort developed
               by Prof Hinsby Cadillo-Quiroz (cadillo lab) aiming to increase the
               participation in microbiology research and high order thinking and
               learning by bridging education and research. Students engage in
               full semester projects of varying nature defined on the previous
-              semester as a group effort.
-            </p>
-            <p>
-              Most projects involved learning about microbial diversity and
-              novel groups of microorganisms. The research is student-centered
-              where senior members train new members. Students are invited to
-              remain in program for two semesters so they can act as trainee and
-              trainer.
-            </p>
-            <p>
-              A common effect is that after one year training students move on
-              to professional jobs or other scientific laboratories where their
-              progress will be further advanced. ASU students feel free to
-              enquire about this at any point!
+              semester as a group effort. Most projects involved learning about
+              microbial diversity and novel groups of microorganisms. The
+              research is student-centered where senior members train new
+              members. Students are invited to remain in program for two
+              semesters so they can act as trainee and trainer. A common effect
+              is that after one year training students move on to professional
+              jobs or other scientific laboratories where their progress will be
+              further advanced. ASU students feel free to enquire about this at
+              any point!
             </p>
           </div>
 
           <div className="programs">
             <h3>A Culturing Soil Microbes for ecosystem models CURE</h3>
-            <p>
+            <p style={{ lineHeight: "1.2" }}>
               This CURE was designed by the Cadillo Lab to promote scientific
               research and training among ASU and international institutions.
               The projects in this class focus on understanding and quantifying
               the role of microorganisms in terrestrial ecosystems like northern
-              forest in Maine or wetlands in the Amazon rainforest.
+              forest in Maine or wetlands in the Amazon rainforest. A common
+              question is whether the variation on phylogeny (membership), types
+              and rates of activity (traits) make some groups to be more, or
+              less influential in an ecological process. For instance, the
+              carbon cycling and greenhouse gas production or consumption (like
+              CO2 and CH4) since they affect climate change. This class (and
+              associated projects) ask students to contribute to microbial
+              knowledge that can help fight climate change. All pedagogical
+              material, as well as support we provide to interested institutions
+              can be found in this link. Please reach us at any point!
             </p>
           </div>
         </section>
 
-        <section className="data-collaboration">
+        <section className="data-collaboration" style={{ marginTop: "3rem" }}>
           <h2>Collaborative Data accessibility and FAIR data use</h2>
-          <p className="para1">
+          <p className="para1" style={{ lineHeight: "1.2" }}>
             The cadillo Lab seeks to be a place where Findable, Accessible,
             Interoperable, and Reusable (FAIR) data access approaches is
-            provided.
+            provided, at the same time we see high value to going beyond making
+            data accessible, we propose to collaborate and further expand the
+            use and reach of previous work through new perspectives and co
+            -analyses. Thus here we provide a list and links to publicly
+            available data, data in development (not yet public), or internally
+            developed data.
           </p>
 
-          <h3>Amazon data:</h3>
           <div className="data-categories">
             <div className="microbial">
               <h4>MICROBIAL</h4>
@@ -118,12 +126,13 @@ const Collaborationss = () => {
         </section>
 
         <section className="international">
-          <h2>
+          <h2 style={{ marginTop: "3rem" }}>
             International Collaborations for Carbon-rich ecosystem research
           </h2>
-          <p className="para2">
+          <p className="para2" style={{ lineHeight: "1.2" }}>
             The Cadillo lab is proud of a long collaborative tradition to study
-            carbon-rich environments around the world.
+            carbon-rich environments around the world. Below find a list of
+            institutions with which we have or currently collaborate:
           </p>
           <ul className="partners-list">
             <li>Universidad Nacional de la Amazonia Peruana (UNAP)</li>
