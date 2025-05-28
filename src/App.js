69c16c3df3c99d50837c302c87193822bb9b94ec
Sanskar Srivastava <113485640+sanskarsri26@users.noreply.github.com>
2025-05-28 22:13:58 +0530

    change in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
 import Research from './components/Research';
@@ -16,6 +15,8 @@ import "./styles/Programs.css";
 import "./styles/Research.css";
 import "./styles/Team.css";
 import Collaborationss from './components/Collaborationss'; 
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
 
 
 const App = () => {
